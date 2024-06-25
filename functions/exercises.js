98415f2ba978fbc5727e4e7867cab9f2d4a70361
Eric S <estriler@hotmail.com>
2024-06-25 16:14:47 -0500

    finished functions exercise

--- functions/exercises.js
+++ functions/exercises.js
@@ -1,17 +1,17 @@
-function makeLine(num){
+function makeLine(num, char){
     let str = "";
     for(let i = 0; i < num; i++){
-        str += "#";
+        str += char;
     }
     return str;
 }
 
-// console.log(makeLine(12));
+// console.log(makeLine(12, "E"));
 
-function makeSquare(size){
+function makeSquare(size, char){
     let square = "";
     for(let i = 0; i < size; i++){
-        square += makeLine(size);
+        square += makeLine(size, char);
      if(i < size-1){
         square += "\n";
      }
@@ -19,12 +19,12 @@ function makeSquare(size){
     return square;
 }
 
-// console.log(makeSquare(7));
+// console.log(makeSquare(7, "R"));
 
-function makeRectangle(width, height){
+function makeRectangle(width, height, char){
     let rectangle = "";
     for(let i = 0; i < height; i++){ 
-        rectangle += makeLine(width);
+        rectangle += makeLine(width, char);
         if(i < height-1){
             rectangle += "\n";
         }
@@ -32,4 +32,58 @@ function makeRectangle(width, height){
     return rectangle;
 }
 
-console.log(makeRectangle(5,3));
+// console.log(makeRectangle(5,3,"I"));
+
+function makeDownwardStairs(height, char){
+    let stairs = "";
+    for(let i = 1; i < height+1; i++){
+        stairs += makeLine(height-(height-i), char)
+        if(i < height){
+            stairs += "\n";
+        }
+    }
+    return stairs;
+}
+
+// console.log(makeDownwardStairs(5,"C"));
+
+function makeSpaceLine(numSpaces, numChars, char){
+    let str = "";
+    for(let i = 0; i < numSpaces; i++){
+        str += " ";
+    }
+    for(let i = 0; i < numChars; i++){
+        str += char;
+    }
+    for(let i = 0; i < numSpaces; i++){
+        str += " ";
+    }
+    return str;
+}
+
+// console.log(makeSpaceLine(3,5, "S"));
+
+function makeIsoscelesTriangle(height, char){
+    let str = "";
+    for(let i = 0; i < height; i++){
+        str += makeSpaceLine((height - i - 1), (2 * i + 1), char);
+        if(i < height-1){
+            str += "\n";
+        }
+    }
+    return str;
+}
+
+// console.log(makeIsoscelesTriangle(5, "T"));
+
+function reverse(str){
+    return str.split('').reverse().join('');
+}
+
+function makeDiamond(height, char){
+    let str = makeIsoscelesTriangle(height, char) + "\n" + reverse(makeIsoscelesTriangle(height, char));
+    return str;
+
+}
+
+console.log(makeDiamond(5, "W"));
